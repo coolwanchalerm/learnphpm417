5ad684531a7eefa1fd8289b69b7cd39144c93f61
coolwanchalerm <wanchalerm@sakolraj.ac.th>
2022-12-23 13:45:01 +0700

    edit inc

--- inc/nav.php
+++ inc/nav.php
@@ -1,9 +1,9 @@
 <nav class="navbar bg-success text-bg-dark">
         <div class="container-fluid">
-          <a href="index.html" class="navbar-brand text-light">ศาลาโสเหล่</a>
+          <a href="index.php" class="navbar-brand text-light">ศาลาโสเหล่</a>
           <div class="d-flex" role="search">
             
-            <a class="btn btn-outline-light" href="post.html">
+            <a class="btn btn-outline-light" href="post.php">
                 <i class="fa-solid fa-comments"></i> สร้างโพสต์ใหม่</a>
           </div>
         </div>
