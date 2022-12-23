b866657e3054b5b9aaeaf140389dc9b1e96eeefd
coolwanchalerm <wanchalerm@sakolraj.ac.th>
2022-12-23 11:15:43 +0700

    inc folder

---
+++ inc/nav.php
@@ -0,0 +1,10 @@
+<nav class="navbar bg-success text-bg-dark">
+        <div class="container-fluid">
+          <a href="index.html" class="navbar-brand text-light">ศาลาโสเหล่</a>
+          <div class="d-flex" role="search">
+            
+            <a class="btn btn-outline-light" href="post.html">
+                <i class="fa-solid fa-comments"></i> สร้างโพสต์ใหม่</a>
+          </div>
+        </div>
+    </nav>
